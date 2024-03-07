48e7c8eeee00cdc19ad5ea63c303bda0ed771fcf
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2024-03-07 11:10:32 -0300

    resposta do desafio de numero 4

---
+++ Desafio_04.java
@@ -0,0 +1,21 @@
+public class Desafio_04 {
+
+    public static void main(String[] args){
+                     // sequencia de procedimentos
+
+
+//        eu vou ligar o interruptor por alguns bons minutos e, depois, vou desliga-lo.
+//       vou deixar o outro  interruptor desligado e o terceiro interruptor vou ligar tambem.
+//        Após alguns minutos, vou ate a sala das lâmpadas.
+//        Se uma lâmpada estiver acesa, isso significa que o interruptor ligado anteriormente controla essa lâmpada.
+//        Se uma lâmpada estiver apagada e ainda estiver fria ao toque,
+//        isso significa que o interruptor desligado anteriormente controla essa lâmpada.
+//        Se uma lâmpada estiver apagada, mas estiver quente ao toque,
+//        isso significa que o interruptor mantido ligado anteriormente, mas que foi desligado, controla essa lâmpada.
+//        Agora, existe apenas um interruptor que não foi testado diretamente.
+//        vou liga-lo  e ir até a sala das lâmpadas.
+//        Se a lâmpada que estava apagada e fria ao toque agora estiver acesa, esse interruptor controla essa lâmpada.
+//        Se a lâmpada que estava acesa anteriormente agora estiver apagada, esse interruptor controla essa lâmpada.
+
+    }
+}
